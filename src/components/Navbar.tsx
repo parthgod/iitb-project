ff72960a5617434982bd8ecede9930b69d851f6f
parthgod <parthgenius.gps@gmail.com>
2024-03-30 20:08:15 +0530

    changes in public folder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="absolute animate-slidedown top-0 w-screen z-[100] h-[9vh] flex items-center justify-between px-5">
       <Image
-        src="/images/logo.png"
+        src="/assets/images/logo.png"
         alt="logo"
         width={200}
         height={200}
